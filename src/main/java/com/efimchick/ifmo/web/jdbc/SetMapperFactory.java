415ed54da6a2f650f2ab62a80bb362c70db1e6c8
Anastasia717 <asburtseva2000@gmail.com>
2020-01-17 17:16:04 +0300

    First try

--- src/main/java/com/efimchick/ifmo/web/jdbc/SetMapperFactory.java
+++ src/main/java/com/efimchick/ifmo/web/jdbc/SetMapperFactory.java
@@ -1,12 +1,56 @@
 package com.efimchick.ifmo.web.jdbc;
 
+import java.math.BigInteger;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.time.LocalDate;
+import java.util.HashSet;
 import java.util.Set;
 
 import com.efimchick.ifmo.web.jdbc.domain.Employee;
+import com.efimchick.ifmo.web.jdbc.domain.FullName;
+import com.efimchick.ifmo.web.jdbc.domain.Position;
 
 public class SetMapperFactory {
 
     public SetMapper<Set<Employee>> employeesSetMapper() {
-        throw new UnsupportedOperationException();
+        return resultSet -> {
+            Set<Employee> employeeSet = new HashSet<>();
+            try {
+                while (resultSet.next())
+                    employeeSet.add(mapRow(resultSet));
+                return employeeSet;
+            } catch (SQLException e) {
+                e.printStackTrace();
+                throw new UnsupportedOperationException();
+            }
+        };
+    }
+
+    private Employee mapRow(ResultSet set) {
+        try {
+            Employee manager = null;
+            if (set.getString("MANAGER") != null) {
+                int managId = set.getInt("MANAGER");
+                int currentRow = set.getRow();
+                set.absolute(0);
+
+                while (set.next())
+                    if (managId == Integer.parseInt(set.getString("ID")))
+                        manager = mapRow(set);
+                set.absolute(currentRow);
+            }
+
+            return new Employee(new BigInteger(set.getString("ID")),
+                    new FullName(set.getString("FIRSTNAME"), set.getString("LASTNAME"), set.getString("MIDDLENAME")),
+                    Position.valueOf(set.getString("POSITION")),
+                    LocalDate.parse(set.getString("HIREDATE")),
+                    set.getBigDecimal("SALARY"),
+                    manager);
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+            throw new UnsupportedOperationException();
+        }
     }
 }
